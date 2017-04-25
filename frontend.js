13737dbb01400623af69202434601b50b13cb26f
Jonathan Brown <jbrown@bluedroplet.com>
2017-04-25 17:57:59 +0700

    Add code.

---
+++ frontend.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var Web3 = require('web3');
+var Eth = require('ethjs');
+var ethUtil = require('ethereumjs-util');
+var $ = require('jquery');
+
+module.exports = {};
+
+module.exports.signIn = function() {
+
+  window.Eth = Eth;
+  window.web3 = new Web3(web3.currentProvider);
+  var eth = new Eth(web3.currentProvider)
+
+  eth.personal_sign(web3.eth.accounts[0], ethUtil.bufferToHex(new Buffer("Sign into demo app.", 'utf8')))
+  .then((signed) => {
+    console.log('Signed!  Result is: ', signed);
+
+    $.ajax({
+      method: 'POST',
+      contentType: 'application/json',
+      url: 'http://localhost:3000/sign-in',
+      data: JSON.stringify({
+        account: web3.eth.accounts[0],
+        signed: signed,
+      }),
+      success: function (data, textStatus, jqXHR) {
+        console.log('Signed in.');
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log('Failed to sign in.');
+      }
+    });
+  })
+}
---
+++ index.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressJwt = require('express-jwt');
+var ethUtil = require('ethereumjs-util');
+var sigUtil = require('eth-sig-util');
+var jwt = require('jsonwebtoken');
+
+module.exports = {};
+
+module.exports.attach = function(app, secret) {
+  app.use(bodyParser.json());
+  app.use(cookieParser());
+
+  app.use(expressJwt({
+    secret: secret,
+    credentialsRequired: false,
+    getToken: function fromHeaderOrQuerystring (req) {
+      return req.cookies.token;
+    }
+  }).unless({path: ['/sign-in']})
+  );
+
+  app.post('/sign-in', function (req, res) {
+    console.log(req.body);
+    
+    var msgParams = {
+      data: ethUtil.bufferToHex(new Buffer("Sign into demo app.", 'utf8')),
+      sig: req.body.signed,
+    };
+    var recovered = sigUtil.recoverPersonalSignature(msgParams)
+
+    if (recovered === req.body.account) {
+      console.log('SigUtil Successfully verified signer as ' + req.body.account);
+      
+      var token = jwt.sign({loggedInAs: req.body.account}, secret);
+      
+      console.log('JWT token: ' + token);
+      res.cookie('token', token, {domain: 'localhost', httpOnly: true});
+      res.end();
+    } else {
+      console.log('SigUtil recover the message signer');
+    }  
+  });
+}
