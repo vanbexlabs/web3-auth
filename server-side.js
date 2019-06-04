48e48ef326410aca85d9abb14461e813f4633072
hp <hp_ctt@hotmail.com>
2019-06-04 18:13:36 +0100

    add server side example

---
+++ server-side.js
@@ -0,0 +1,64 @@
+"use strict";
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressJwt = require('express-jwt');
+var ethUtil = require('ethereumjs-util');
+var sigUtil = require('eth-sig-util');
+var jwt = require('jsonwebtoken');
+
+module.exports = {};
+
+module.exports.attach = function (app, secret) {
+  // Don't accept non-AJAX requests to prevent XSRF attacks.
+  app.use(function (req, res, next) {
+    // if (!req.xhr) {
+    //   res.status(500).send('Not AJAX');
+    // } else {
+    //   next();
+    // }
+    next();
+  });
+
+  app.use(bodyParser.json());
+  app.use(cookieParser());
+
+  app.use(expressJwt({
+    secret: secret,
+    credentialsRequired: false,
+    getToken: function fromHeaderOrQuerystring(req) {
+      return req.cookies.token;
+    }
+  }).unless({
+    path: ['/sign-in']
+  }));
+
+  app.post('/sign-in', function (req, res) {
+    console.log(req.body);
+
+    var baseUrl = req.protocol + "://" + req.hostname;
+
+    var msgParams = {
+      data: ethUtil.bufferToHex(new Buffer("Sign into " + baseUrl, 'utf8')),
+      sig: req.body.signed,
+    };
+    var recovered = sigUtil.recoverPersonalSignature(msgParams)
+
+    if (recovered === req.body.account) {
+      console.log('SigUtil Successfully verified signer as ' + req.body.account);
+
+      var token = jwt.sign({
+        loggedInAs: req.body.account
+      }, secret);
+
+      console.log('JWT token: ' + token);
+      res.cookie('token', token, {
+        domain: 'localhost',
+        httpOnly: true
+      });
+      res.end();
+    } else {
+      console.log('SigUtil unable to recover the message signer');
+    }
+  });
+}
